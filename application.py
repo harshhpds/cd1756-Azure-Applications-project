2c81f8d23fd5eb2bba37b50872df4ed7d79107c2
HarshA-3003 <harshabhang@gmail.com>
2026-01-12 19:16:31 +0530

    Completed MS login, logging, and Azure-ready config

--- application.py
+++ application.py
@@ -1,14 +1,11 @@
 """
-This script runs the FlaskWebProject application using a development server.
+This script runs the FlaskWebProject application using Azure App Service.
 """
 
-from os import environ
+import os
 from FlaskWebProject import app
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+if __name__ == "__main__":
+    # Use Azure's assigned port and bind to all interfaces
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
