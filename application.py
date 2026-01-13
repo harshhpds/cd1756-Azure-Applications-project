67e1855ec1ce2d43a50005f6d18423f271800c39
HarshA-3003 <harshabhang@gmail.com>
2026-01-13 20:36:59 +0530

    Completed MS login, logging, and Azure-ready config

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 """
-Azure entry point for Gunicorn.
-Do NOT call app.run() here.
+This script runs the FlaskWebProject application locally.
 """
 
 from FlaskWebProject import app
+
+if __name__ == "__main__":
+    # For testing locally
+    app.run(host="0.0.0.0", port=5555, debug=True)
